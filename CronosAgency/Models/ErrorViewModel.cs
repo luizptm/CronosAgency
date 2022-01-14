749d811c00fc1a699dca61a5ea138acd5b8a64ba
Luiz Paulo Toniazzo Martins <luiz.paulo@fgv.br>
2022-01-14 17:36:01 -0300

    Adicionar arquivos de projeto.

---
+++ CronosAgency/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CronosAgency.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
